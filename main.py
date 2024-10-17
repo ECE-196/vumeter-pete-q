c79952ff74420e351bea6b6c9f91211bd86768a8
pete-q <pquawas@gmail.com>
2024-10-16 21:33:01 -0700

    Main finished

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -21,17 +28,33 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+# Initializing variables
+filtered_volume = 0
+alpha_rise = 0.8
+alpha_fall = 0.1
+min_volume = microphone.value  # Initialize 0 value
+max_volume = 50000   # Max expected volume
+
 # main loop
 while True:
     volume = microphone.value
 
-    print(volume)
+    # Apply exponential filter:
+    # Quickly rise when volume increases
+    # Slowly fall when volume decreases
+    if volume > filtered_volume:
+        filtered_volume = alpha_rise * volume + (1 - alpha_rise) * filtered_volume
+    else:
+        filtered_volume = alpha_fall * volume + (1 - alpha_fall) * filtered_volume
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    print(f"Raw Volume: {volume}, Filtered Volume: {filtered_volume}")
 
-    sleep(1)
+    # Control the LEDs based on the filtered volume
+    for i in range(11):
+        threshold = (i + 1) * max_volume / 11
+        if filtered_volume > threshold:
+            leds[i].value = 1
+        else:
+            leds[i].value = 0
 
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    sleep(0.1)
